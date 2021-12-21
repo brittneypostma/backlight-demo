7896cea728ae5b7ce528ac4dab877ac97b46a039
brittneypostma <brittney.postma@hotmail.com>
2021-12-21 12:09:21 -0500

    fixed imports and link

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '@backlight-dev/backlight-svelte-tutorial.svelte-kxf3qlgg/main/dist/style.css'
+  import SwitchTheme from '@backlight-dev/backlight-svelte-tutorial.svelte-kxf3qlgg/switch-theme/src/SwitchTheme.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<SwitchTheme />
